a1af8b11324a965e87b46764bf25ed3fa9352bfd
kobecow <sneeiwji@gmail.com>
2019-11-27 06:29:23 -0500

    modify chatbot

--- functions/sendToLearn.js
+++ functions/sendToLearn.js
@@ -7,7 +7,33 @@ module.exports = function (){
         },
         message: {
             text: `Sorry I can't answer it right now. But I will answer against " ${recivedMessage} ".
-            It means This bot can learn new things from you every day.`
+It means This bot can learn new things from you every day`,
+        quick_replies: [{
+                    "content_type":"text",
+                    "title":"Experiences",
+                    "payload":"Experiences"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Portfolios",
+                    "payload":"Portfolios"
+                },
+                {
+                    "content_type":"text",
+                    "title":"General Info",
+                    "payload":"General Info"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Skills",
+                    "payload":"Skills"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Other",
+                    "payload":"Other"
+                }
+            ]
         }
     };
     // TODO
