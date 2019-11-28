6c4067322e0284cc68a82e89d4b2e2ae1ee11e4d
kobecow <sneeiwji@gmail.com>
2019-11-28 12:17:11 -0500

    a

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post('/webhook', (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
   console.dir(body);
-  let graphURL = `https://graph.facebook.com/v5.0/{body.entry[0].id}`;
+  let graphURL = `https://graph.facebook.com/v5.0/${body.entry[0].id}`;
   
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
